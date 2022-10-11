0ac2728efb05899b2b31b45563dfd499bf9b5486
Emil Bodholdt Nielsen <idvx3230@easjdk.onmicrosoft.com>
2022-10-11 18:11:22 +0200

    New commit

---
+++ Storebaelt/Managers/StorebaeltManager.cs
@@ -0,0 +1,20 @@
+﻿namespace Storebaelt.Managers {
+    public class StorebaeltManager {
+        
+        private static List<Ticket> _tickets = new List<Ticket>()
+        {
+            new Ticket(){LicensePlate}
+        }
+
+        public StorebaeltManager()
+        {
+            
+        }
+
+        public Storebaelt Create(Ticket ticket)
+        {
+
+        }
+
+    }
+}
